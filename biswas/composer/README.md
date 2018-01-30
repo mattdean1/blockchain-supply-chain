81b9b11140e87cfc75d97fbc8700f638e6875d3b
mattdean1 <matt.dean.001@gmail.com>
2018-01-30 16:09:52 +0000

    Add Biswas composer boilerplate

---
+++ biswas/composer/README.md
@@ -0,0 +1,1 @@
+# org1.example.com
---
+++ biswas/composer/lib/logic.js
@@ -0,0 +1,23 @@
+'use strict';
+/**
+ * Write your transction processor functions here
+ */
+
+/**
+ * Sample transaction
+ * @param {org1.example.com.ChangeAssetValue} changeAssetValue
+ * @transaction
+ */
+function onChangeAssetValue(changeAssetValue) {
+    var assetRegistry;
+    var id = changeAssetValue.relatedAsset.assetId;
+    return getAssetRegistry('org1.example.com.SampleAsset')
+        .then(function(ar) {
+            assetRegistry = ar;
+            return assetRegistry.get(id);
+        })
+        .then(function(asset) {
+            asset.value = changeAssetValue.newValue;
+            return assetRegistry.update(asset);
+        });
+}
