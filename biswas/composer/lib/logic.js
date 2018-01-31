cecbdb8ab06561ad8241e2b7d3f514f43b4dba9d
mattdean1 <matt.dean.001@gmail.com>
2018-01-31 14:46:13 +0000

    Add Biswas' grower and producer models

--- biswas/composer/lib/logic.js
+++ biswas/composer/lib/logic.js
@@ -4,20 +4,20 @@
  */
 
 /**
- * Sample transaction
- * @param {org1.example.com.ChangeAssetValue} changeAssetValue
+ * Transfers some quantity of a resource (e.g. grapes, wine bottles)
+ * from one actor to another
+ * @param {biswas.base.SellGrapes} sale
  * @transaction
  */
-function onChangeAssetValue(changeAssetValue) {
-    var assetRegistry;
-    var id = changeAssetValue.relatedAsset.assetId;
-    return getAssetRegistry('org1.example.com.SampleAsset')
-        .then(function(ar) {
-            assetRegistry = ar;
-            return assetRegistry.get(id);
-        })
-        .then(function(asset) {
-            asset.value = changeAssetValue.newValue;
-            return assetRegistry.update(asset);
-        });
+function sellGrapes(sale) {
+    const { grapes, buyer, quantityToBeSold } = sale
+    // verify that the grapes can be sold
+        // quantity is ok
+        // participant is the owner of the batch
+        // vineyard is owned by grapegrower
+        
+    // create a new batch of grapes
+
+    // decrement original grapes quantity
+        // if 0 then 
 }
