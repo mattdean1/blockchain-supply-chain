2f88a9efbfcea7f9039669e33d4fed53c67e8283
mattdean1 <matt.dean.001@gmail.com>
2018-02-06 13:07:42 +0000

    Convert tx processing scripts to ES5

--- biswas/composer/lib/grower.js
+++ biswas/composer/lib/grower.js
@@ -1,5 +1,6 @@
 'use strict';
-const uuid = require('uuid/v4');
+
+var uuid = require('uuid/v4');
 
 /**
  * Transfers some quantity of a resource (e.g. grapes, wine bottles)
@@ -7,41 +8,47 @@ const uuid = require('uuid/v4');
  * @param {biswas.grower.SellGrapes} sale
  * @transaction
  */
-const sellGrapes = (sale) => {
-    const { grapes, buyer, quantityToBeSold } = sale
-    const growerNamespace = 'biswas.grower'
-    
+function sellGrapes(sale) {
+    var grapes = sale.grapes;
+    var buyer = sale.buyer;
+    var quantityToBeSold = sale.quantityToBeSold;
+    var growerNamespace = 'biswas.grower';
+
     // verify that the grapes can be sold
     if (quantityToBeSold > grapes.quantity) {
-        throw new Error('Batch of grapes is too small')
+        throw new Error('Batch of grapes is too small');
     }
-    const grower = getCurrentParticipant()
-    if(grapes.owner !== grower || grapes.grapeGrower !== grower) {
-        throw new Error('You do not own those grapes')
+    var grower = getCurrentParticipant();
+    if (grapes.owner !== grower || grapes.grapeGrower !== grower) {
+        throw new Error('You do not own those grapes');
     }
     if (!grower.vineyards || !grower.vineyards.includes(grapes.vineyard)) {
-        throw new Error('Those grapes are not from your vineyard')
+        throw new Error('Those grapes are not from your vineyard');
     }
     // check buyer exists?
-    
-    let grapesRegistry;
+
+    var grapesRegistry;
     return getAssetRegistry(growerNamespace + '.Grapes')
-        .then(grapesReg => {
-            grapesRegistry = grapesReg
+        .then(function(grapesReg) {
+            grapesRegistry = grapesReg;
             // create a new batch of grapes for the new owner
-            const id = `GRAPES_${uuid()}`;
-            let newGrapes = getFactory().newResource(growerNamespace, 'Grapes', id)
+            var id = 'GRAPES_' + uuid();
+            var newGrapes = getFactory().newResource(
+                growerNamespace,
+                'Grapes',
+                id
+            );
             newGrapes = Object.assign(newGrapes, grapes, {
                 id: id,
                 quantity: quantityToBeSold,
                 owner: buyer
-            })
-            return grapesRegistry.add(newGrapes)
+            });
+            return grapesRegistry.add(newGrapes);
         })
-        .then(() => {
+        .then(function() {
             // decrement the quantity of the batch sold
-            const newQuantity = grapes.quantity - quantityToBeSold
-            grapes.quantity = newQuantity
-            return grapesRegistry.update(grapes)
-        })
+            var newQuantity = grapes.quantity - quantityToBeSold;
+            grapes.quantity = newQuantity;
+            return grapesRegistry.update(grapes);
+        });
 }
--- biswas/composer/lib/producer.js
+++ biswas/composer/lib/producer.js
@@ -1,10 +1,11 @@
 'use strict';
-const uuid = require('uuid/v4');
+var uuid = require('uuid/v4');
 
-
-const grapesToWine = (grapesQuantity) => {
-    // Convert the units of grapes to wine
-    return grapesQuantity / 2
+/**
+ * Convert the units of grapes to wine
+ */
+function grapesToWine(grapesQuantity) {
+    return grapesQuantity / 2;
 }
 
 /**
@@ -13,41 +14,44 @@ const grapesToWine = (grapesQuantity) => {
  * @param {biswas.producer.CreateWine} create
  * @transaction
  */
-const createWine = (create) => {
+function createWine(create) {
     // check grapes are owned by producer submitting transaction
-    const { grapes } = create
-    const factory = getFactory()
-    const NS = 'biswas'
-    const producerNamespace = NS + 'producer'
-    const growerNamespace = NS + 'grower'
+    var grapes = create.grapes;
+    var factory = getFactory();
+    var NS = 'biswas';
+    var producerNamespace = NS + 'producer';
+    var growerNamespace = NS + 'grower';
 
-    const producer = getCurrentParticipant()
+    var producer = getCurrentParticipant();
     if (grapes.owner !== producer) {
-        throw new Error('You do not own those grapes')
+        throw new Error('You do not own those grapes');
     }
 
     return getAssetRegistry(producerNamespace + '.BulkWine')
-        .then(bwReg => {
+        .then(function(bwReg) {
             // Create the BulkWine
-            const id = `BULKWINE_${uuid()}`;
-            let bulkWine = getFactory().newResource(producerNamespace, 'BulkWine', id)
+            var id = 'BULKWINE_' + uuid();
+            var bulkWine = getFactory().newResource(
+                producerNamespace,
+                'BulkWine',
+                id
+            );
             Object.assign(bulkWine, {
-                grapes,
-                producer,
+                grapes: grapes,
+                producer: producer,
                 owner: producer,
                 quantity: grapesToWine(grapes.quantity)
-            })
+            });
 
-            return bwReg.add(bulkWine)
+            return bwReg.add(bulkWine);
         })
-        .then(() => {
-            return getAssetRegistry(growerNamespace + 'Grapes')
+        .then(function() {
+            return getAssetRegistry(growerNamespace + 'Grapes');
         })
-        
-        .then(grapesRegistry => {
+
+        .then(function(grapesRegistry) {
             // Consume the grapes - assume the entire batch is used
-            grapes.quantity = 0
-            grapesRegistry.update(grapes)
-        })
-
+            grapes.quantity = 0;
+            grapesRegistry.update(grapes);
+        });
 }
