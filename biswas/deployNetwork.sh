02f082217c885649e9abccee9602a175d72c3974
mattdean1 <matt.dean.001@gmail.com>
2018-02-21 14:53:26 +0000

    Add scripts for end-to-end (re)deployment of business network

---
+++ biswas/deployNetwork.sh
@@ -0,0 +1,34 @@
+# Compile composer network from source
+composer archive create --sourceType dir --sourceName ./composer --archiveFile ./composer/biswas.bna
+
+# Start fabric and create channel
+export COMPOSE_PROJECT_NAME=biswas
+./restartFabric.sh
+docker exec cli /bin/sh "./createChannel.sh"
+
+# Install composer runtime onto peers
+composer runtime install -c PeerAdmin@biswas-grower-only -n biswas
+composer runtime install -c PeerAdmin@biswas-producer-only -n biswas
+
+# Install the business network and start it
+composer network start\
+ -c PeerAdmin@biswas-grower-only\
+ -a composer/biswas.bna\
+ -A grower-network-admin\
+ -C fabric/id-cards/grower-network-admin/admin-pub.pem\
+ -A producer-network-admin\
+ -C fabric/id-cards/producer-network-admin/admin-pub.pem
+
+# Ping network to check it's up
+echo "Expect the first ping to fail..."
+composer network ping -c grower-network-admin@biswas
+sleep 5
+composer network ping -c grower-network-admin@biswas
+
+composer network ping -c producer-network-admin@biswas
+sleep 5
+composer network ping -c producer-network-admin@biswas
+
+# Remove unnecessary ID cards created as part of the deploy process
+rm ./grower-network-admin@biswas.card
+rm ./producer-network-admin@biswas.card
---
+++ biswas/restartFabric.sh
@@ -0,0 +1,2 @@
+docker-compose -f ./fabric/docker/docker-compose-sdk.yaml down
+docker-compose -f ./fabric/docker/docker-compose-sdk.yaml up -d
